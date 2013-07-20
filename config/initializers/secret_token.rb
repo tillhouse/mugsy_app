8d1fefdcc6ae6ea6bdd433a263dc123a2101b3ee
Jeremy R. Tillman <jeremy.r.tillman@gmailcom>
2013-07-19 22:31:45 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MugsyApp::Application.config.secret_key_base = '277e7dfababbdf88a477e53bdc2b1a7629d6f261ed4df33e8f3c51640fe62784ee97ab79e55d86cf49386eccf136010de372ee7efdf1c4feec670e6eee7a22b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MugsyApp::Application.config.session_store :cookie_store, key: '_mugsy_app_session'
